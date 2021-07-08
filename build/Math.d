fadd3119ca1dacad9762d00fc469f2bc854797a0
Wilderness1265 <66174169+Wilderness1265@users.noreply.github.com>
2021-07-08 11:04:51 -0400

    Delete build directory

--- build/Math.d
+++
@@ -1,4 +0,0 @@
-Math.o: \
- /home/wilder/Desktop/Derricks-Stuff/Languages/C/Homebrew/Game/source/Math.c \
- /home/wilder/Desktop/Derricks-Stuff/Languages/C/Homebrew/Game/source/Math.h
-/home/wilder/Desktop/Derricks-Stuff/Languages/C/Homebrew/Game/source/Math.h:
